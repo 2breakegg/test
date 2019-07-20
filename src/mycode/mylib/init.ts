cf7c9bea7bbb5767c9a86760c8aa2c0a8c79d374
break_egg <1216023881@qq.com>
2019-07-20 15:17:50 +0800

    init.ts

--- src/mycode/mylib/init.ts
+++ src/mycode/mylib/init.ts
@@ -1,14 +1,17 @@
 import * as THREE from "three";
 import EventFunctions from "./eventfunctions";
+import Stats from "../jsm/libs/stats.module";
 
 // tslint:disable-next-line: prefer-const
 let scene = new THREE.Scene(); // 创建场景
 let camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 2000); // 创建相机 (参数分别是   FOV:可视角度,  aspect ratio:宽高比,  near:近剪切面,  far:远剪切面)
 let renderer =  new THREE.WebGLRenderer(); // 创建渲染器
+let stats: any = new Stats(); // 显示帧数
 
 renderer.setSize(window.innerWidth, window.innerHeight); // 设置渲染器的宽高
 document.body.appendChild(renderer.domElement); // 将渲染器的dom添加进body中
 renderer.render(scene, camera); // 将场景和相机交给渲染器进行渲染
+document.body.appendChild( stats.dom );
 
 camera.position.z = 10;
 renderer.render(scene, camera);
@@ -20,6 +23,7 @@ function animate() {
     requestAnimationFrame(animate);
     animateFunctions.runAll();
     renderer.render(scene, camera);
+    stats.update(); // 刷新帧数
 }
 
 animate();
--- src/mycode/start/start_point.ts
+++ src/mycode/start/start_point.ts
@@ -1,14 +1,7 @@
 import {scene, camera, animateFunctions} from "../mylib/init";
 import * as THREE from "three";
-import Stats from "../jsm/libs/stats.module";
-
-let stats: any = new Stats();
-
-document.body.appendChild( stats.dom );
 
 function aminate() {
-    stats.update();
-    // cube.position.y -= 0.01;
 }
 
 animateFunctions.add(aminate);
